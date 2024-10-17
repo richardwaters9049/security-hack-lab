3e793027ddab65d4e1f9c97af877f47e56e1bba1
richardwaters9049 <119029825+richardwaters9049@users.noreply.github.com>
2024-10-17 11:02:44 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}", // Updated to point to the public directory
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'media', // Can keep or change as needed
+  plugins: [],
+}
